19c065e89c7aba3535d7601bc12b1738df8ac831
Arshita4 <ap8158@nyu.edu>
2025-02-27 03:40:42 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_lab2digitallogicq2 (
+  tt_um_lab2digitallogicq2 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
