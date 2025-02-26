9be7e3cc1ef0f2424ffb8e813097685d6edbe030
Arshita4 <ap8158@nyu.edu>
2025-02-26 10:14:02 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -24,20 +24,20 @@ module tt_um_lab2digitallogicq2 (
         if (In[15])==1
           begin
             if (In[14]) C==8'b00001110;
-            elif (In[13]) C==8'b00001101;
-            elif (In[12]) C==8'b00001100;
-            elif (In[11]) C==8'b00001011;
-            elif (In[10]) C==8'b00001010;
-            elif (In[9]) C==8'b00001001;
-            elif (In[8]) C==8'b00001000;
-            elif (In[7]) C==8'b00000111;
-            elif (In[6]) C==8'b00000110;
-            elif (In[5]) C==8'b00000101;
-            elif (In[4]) C==8'b00000100;
-            elif (In[3]) C==8'b00000011;
-            elif (In[2]) C==8'b00000010;
-            elif (In[1]) C==8'b00000001;
-            elif (In[0]) C==8'b00000000;
+            else if (In[13]) C==8'b00001101;
+            else if (In[12]) C==8'b00001100;
+            else if (In[11]) C==8'b00001011;
+            else if (In[10]) C==8'b00001010;
+            else if (In[9]) C==8'b00001001;
+            else if (In[8]) C==8'b00001000;
+            else if (In[7]) C==8'b00000111;
+            else if (In[6]) C==8'b00000110;
+            else if (In[5]) C==8'b00000101;
+            else if (In[4]) C==8'b00000100;
+            else if (In[3]) C==8'b00000011;
+            else if (In[2]) C==8'b00000010;
+            else if (In[1]) C==8'b00000001;
+            else if (In[0]) C==8'b00000000;
             else
             C=11110000;
           
